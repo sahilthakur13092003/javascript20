6c5950e489d7ec2f7ec12fba3119c3fca0db6314
Sahil Thakur <97108297+sahilthakur13092003@users.noreply.github.com>
2023-10-20 12:46:49 +0000

    confusion

---
+++ js2023/conversionconfusiondatatype.js
@@ -0,0 +1,10 @@
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+// console.log(typeof stringnumber)
+
+let isbooleanid = 1
+
+let numbId = Boolean(isbooleanid)
+console.log( numbId)
+
--- js2023/variables.js
+++ js2023/variables.js
@@ -2,7 +2,7 @@ const accountId = 232344
 let accountEmail = "sahil@accountEmail.com"
 var accountPass = 23445566
 let accountLocation;
-
+//  const let var = keywords
  accountEmail = "thakur123@accountEmail.com"
 accountPass = 666666
 console.log(accountEmail)
