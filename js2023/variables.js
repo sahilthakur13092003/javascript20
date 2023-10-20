2fda2e53f152bf53cb605e4f5821e6c0eb2d2138
Sahil Thakur <97108297+sahilthakur13092003@users.noreply.github.com>
2023-10-20 07:50:43 +0000

    variablrs

---
+++ js2023/variables.js
@@ -0,0 +1,9 @@
+const accountId = 232344
+let accountEmail = "sahil@accountEmail.com"
+var accountPass = 23445566
+let accountLocation;
+
+ accountEmail = "thakur123@accountEmail.com"
+accountPass = 666666
+console.log(accountEmail)
+console.table([ accountEmail,accountId,accountPass,accountLocation])
