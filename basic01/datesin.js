4117991bfe6b660ac0b2cdd4bf334ec1901efc9c
Sahil Thakur <97108297+sahilthakur13092003@users.noreply.github.com>
2023-10-24 15:29:56 +0000

    datesin js

---
+++ basic01/datesin.js
@@ -0,0 +1,10 @@
+// let mydate = new Date()
+// console.log(mydate)
+
+let mydate = new Date()
+console.log(mydate.toString())
+console.log(mydate.getFullYear())
+console.log(mydate.toLocaleString())
+ 
+let createdate = new Date("09-13-2003")
+console.log(createdate)
