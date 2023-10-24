353db2f2dcc9632cfaccb3d82e707fe1d9fd6f80
Sahil Thakur <97108297+sahilthakur13092003@users.noreply.github.com>
2023-10-24 07:18:37 +0000

    memories

---
+++ js2023/stackandheapmemory.js
@@ -0,0 +1,18 @@
+// stack memory  : primitive datatypes
+// heap memory : non primitive refrences
+
+let myyoutubechannel ="himalayavideos"
+ let anotheryoutubechannel = myyoutubechannel;
+
+anotheryoutubechannel = "sahilvlogsvideos"
+console.log(myyoutubechannel)
+console.log(anotheryoutubechannel)
+
+let myobj={
+    email:"sahil@gmail.com",
+    pass:"1234456"
+}
+let anotherobj = myobj
+anotherobj.email="thakur@gmail.com"
+console.log(myobj.email)
+console.log(anotherobj.email)
